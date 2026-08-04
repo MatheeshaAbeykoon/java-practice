f7a78c3f8753974ec404f7027924ebb6711fb96d
Matheeesha Abeykoon <matheeshaabeykon100@gmail.com>
2026-08-04 12:07:52 +0530

    PositiveNegative.java

---
+++ Week-02/PositiveNegative.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println("Positive Number");
+        } else if (number < 0) {
+            System.out.println("Negative Number");
+        } else {
+            System.out.println("Zero");
+        }
+
+        input.close();
+    }
+}
