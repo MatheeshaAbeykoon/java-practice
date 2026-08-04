64b685f31a16b3e7da0fef43208c2e9607d2b94e
Matheeesha Abeykoon <matheeshaabeykon100@gmail.com>
2026-08-04 12:06:05 +0530

    GradeCalculator.java

---
+++ Week-02/GradeCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Marks: ");
+        int marks = input.nextInt();
+
+        if (marks >= 75) {
+            System.out.println("Grade A");
+        } else if (marks >= 65) {
+            System.out.println("Grade B");
+        } else if (marks >= 55) {
+            System.out.println("Grade C");
+        } else if (marks >= 35) {
+            System.out.println("Grade S");
+        } else {
+            System.out.println("Grade F");
+        }
+
+        input.close();
+    }
+}
