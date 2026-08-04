d8a9c4947c0660bbc2fa1a4c31edd817db86c508
Matheeesha Abeykoon <matheeshaabeykon100@gmail.com>
2026-08-04 11:56:36 +0530

    AreaCalculator.java

---
+++ Week-01/AreaCalculator.java
@@ -0,0 +1,14 @@
+public class AreaCalculator {
+
+    public static void main(String[] args) {
+
+        double length = 10;
+        double width = 5;
+
+        double area = length * width;
+
+        System.out.println("Area = " + area);
+
+    }
+
+}
