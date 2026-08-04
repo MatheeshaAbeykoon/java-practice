6d0da870a64b98e91923081979b9829f6c682abb
Matheeesha Abeykoon <matheeshaabeykon100@gmail.com>
2026-08-04 11:47:05 +0530

    StudentProfile.java

---
+++ StudentProfile.java
@@ -0,0 +1,10 @@
+public class StudentProfile {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name : Matheesha Abeykoon");
+        System.out.println("University : Sabaragamuwa University");
+        System.out.println("Course : Computing and Information Systems");
+    }
+
+}
